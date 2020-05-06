ae1251baf39165ef0a19682043863fb59ded648c
Wadih Ojeil <wadih.ojeil@gmail.com>
2020-05-06 18:45:36 -0400

    getting started

---
+++ src/components/Main.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+
+class Main extends React.Component {
+
+    state ={
+        employee:[]
+    }
+
+
+
+    render() {
+
+        return (
+            <h1>Main Componenet</h1>
+        )
+
+
+
+    }
+
+
+
+};
+
+export default Main;
