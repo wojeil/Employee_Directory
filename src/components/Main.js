db73ef427635b08cce8e592f9970dc39451b2f32
Wadih Ojeil <wadih.ojeil@gmail.com>
2020-05-06 20:46:30 -0400

    table has been created and data is being rendered

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,34 +3,69 @@ import API from "../utils/API"
 
 class Main extends React.Component {
 
-    state ={
-        employees:[]
+    state = {
+        employees: []
     }
 
-    componentDidMount(){
+    componentDidMount() {
         API.getEmployees()
-        .then(data =>{
-            console.log(data.data.results);
-            this.setState({
-                employees:data.data.results
+            .then(data => {
+                console.log(data.data.results);
+                this.setState({
+                    employees: data.data.results
+                })
             })
-        })
-        .catch(err =>console.log (err));
+            .catch(err => console.log(err));
     }
 
 
 
     render() {
 
+
+
+
+
         return (
             <>
-            <h1>Main Componenet</h1>
-            <ul>
-                {this.state.employees.map((employee, i )=> (
-                 <li 
-                 key ={i}>{employee.name.first}</li>   
-                ))}
-            </ul>
+                <div className="jumbotron jumbotron-fluid">
+                    <div className="container">
+                        <h1 className="display-4">Employee Directory</h1>
+                        <p className="lead">Find your Employees</p>
+                        <input className="text-center" placeholder="Search by Name"></input>
+                    </div>
+
+                </div>
+
+                <table className="table table-dark">
+                    <thead>
+                        <tr>
+                            <th scope="col"></th>
+                            <th scope="col">Image</th>
+                            <th scope="col">First</th>
+                            <th scope="col">Last</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Phone Number</th>
+                            <th scope="col">Age</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.employees.map((employee, i) => (
+                            <tr key={i}>
+                                <th scope="row">{i}</th>
+                                <td><img alt={employee.name.first} src={employee.picture.medium} /></td>
+                                <td>{employee.name.first}</td>
+                                <td>{employee.name.last}</td>
+                                <td>{employee.email}</td>
+                                <td>{employee.cell}</td>
+                                <td>{employee.dob.age}</td>
+                                
+                            </tr>
+                        ))}
+
+                    </tbody>
+
+                </table>
             </>
         )
 
